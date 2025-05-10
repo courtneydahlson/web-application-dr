a22e44bbddc7710cb9bf250dbc09bcb7d9aeb147
Courtney Dahlson <courtneydahlson@gmail.com>
2025-05-10 17:43:12 -0400

    Changed s3 policy

--- terraform-frontend/main.tf
+++ terraform-frontend/main.tf
@@ -160,8 +160,8 @@ resource "aws_iam_role" "ec2_s3_access" {
   })
 }
 
-resource "aws_iam_policy" "s3_access_policy" {
-  name = "ec2-s3-access-policy-tf"
+resource "aws_iam_policy" "ec2_s3_access_policy" {
+  name = "s3-access-policy-tf"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -183,7 +183,7 @@ resource "aws_iam_policy" "s3_access_policy" {
 
 resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
   role       = aws_iam_role.ec2_s3_access.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
+  policy_arn = aws_iam_policy.ec2_s3_access_policy.arn
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
